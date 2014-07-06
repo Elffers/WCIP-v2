fc7f814858944301f29d0e50ac986d133388b1c2
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-06 16:21:39 -0700

    parse coords and placemarks init tests pass

--- kml_parser.rb
+++ kml_parser.rb
@@ -3,11 +3,27 @@ require "nokogiri"
 class Parser
   def parse(document)
     data = {}
-    placemarks = []
-    data[:data] = placemarks
     @doc = Nokogiri::XML(File.open(document))
     pms = @doc.css("Placemark")
-    p pms
+    data[:data] = pms.map do |placemark|
+      parse_placemark(placemark)
+    end
     return data
+  end
+
+  def parse_placemark(placemark)
+    # parses Nokogiri::XML::Element 
+    data = {}
+    data[:id] = placemark.at("name").text
+    data[:coords] = parse_coords placemark.at("coordinates").text
+    p data
+    data 
+  end
+
+  def parse_coords(coordinates)
+    c = coordinates.strip
+    c = c.split(/\s/)
+    c.map { |string| string.split(",") }.map {|pair| pair.reverse }
+
   end
 end
--- kml_parser_spec.rb
+++ kml_parser_spec.rb
@@ -2,6 +2,8 @@ require_relative "kml_parser"
 
 describe Parser do
   let(:parser){ Parser.new }
+  let(:coords) { " -122.354206,47.734092 -122.354200,47.732372 -122.354248,47.732323 -122.354303,47.732302" }
+
   context 'parse' do
     it 'returns a hash' do
       expect(parser.parse('sample.kml')).to be_an_instance_of Hash
@@ -13,6 +15,20 @@ describe Parser do
     it 'should contain an array of hashes' do
       expect(parser.parse('sample.kml')[:data].first).to be_an_instance_of Hash
     end
-   
+  end
+
+  context 'parse placemark' do
+    it 'returns a hash' do
+      placemark = Nokogiri::XML(File.open('sample.kml')).css("Placemark").first
+      expect(parser.parse_placemark(placemark)).to be_an_instance_of Hash
+    end
+  end 
+
+  context 'parse coordinates' do
+    it 'returns array with lat first then long' do
+      expect(parser.parse_coords(coords)).to eq [["47.734092", "-122.354206"], ["47.732372", "-122.354200"], ["47.732323", "-122.354248"], ["47.732302", "-122.354303"]]
+    end
   end
 end
+
+
